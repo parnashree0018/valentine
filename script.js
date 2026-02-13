94de441fc9c01eaa23ba17c933eaf4f54b0289a2
parnashree0018 <123476752+parnashree0018@users.noreply.github.com>
2026-02-13 19:04:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,24 +17,14 @@ yesBtn.addEventListener("click", () => {
 noBtn.addEventListener("click", () => {
 
     // shrink NO
-    noScale -= 0.1;
-    if (noScale < 0.3) noScale = 0.3;
+    noScale -= 0.15;
+    if (noScale < 0.2) noScale = 0.2;
     noBtn.style.transform = `scale(${noScale})`;
 
     // grow YES
-    yesScale += 0.15;
-    if (yesScale > 3) yesScale = 3;
+    yesScale += 0.2;
+    if (yesScale > 4) yesScale = 4;
     yesBtn.style.transform = `scale(${yesScale})`;
-
-    // move NO randomly on screen
-    const maxX = window.innerWidth - noBtn.offsetWidth;
-    const maxY = window.innerHeight - noBtn.offsetHeight;
-
-    const randomX = Math.random() * maxX;
-    const randomY = Math.random() * maxY;
-
-    noBtn.style.left = randomX + "px";
-    noBtn.style.top = randomY + "px";
 });
 
 /* PASSWORD CHECK */
